7729c801c0b9643e2f1466394a2be3a6ed23c373
Johan Revilla <johan_revilla_92@hotmail.com>
2024-03-22 10:11:53 -0300

    ejercicio 3.3 informe.py terminado

--- 03_Contenedores_y_Errores/informe.py
+++ 03_Contenedores_y_Errores/informe.py
@@ -12,5 +12,21 @@ def leer_camion(filename):
                 raise "Value incorrect"
     return truck
 
+def leer_precios(filename):
+    prices = []
+    with open(filename, "rt") as file:
+        rows = csv.reader(file)
+        for row in rows:
+            try:
+                prices.append({row[0]: float(row[1])})
+            except ValueError:
+                raise "Value incorrect"
+            except IndexError:
+                break
+
+    return prices
+
 truck = leer_camion('../data/camion.csv')
 print(truck)
+prices = leer_precios('../data/precios.csv')
+print(prices)
