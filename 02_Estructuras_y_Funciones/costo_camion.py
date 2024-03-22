48a61ef8b559e5c1d74d9416b777a1fe3bf4f382
Johan Revilla <johan_revilla_92@hotmail.com>
2024-03-21 21:57:47 -0300

    ejercicio 2.2 costo_camion.py terminado

---
+++ 02_Estructuras_y_Funciones/costo_camion.py
@@ -0,0 +1,15 @@
+FILENAME = "camion.csv"
+
+def costo_camion(filename):
+    cost = 0
+    with open(filename, "rt", encoding="utf-8") as camion_file:
+        next(camion_file)
+
+        for row in camion_file:
+            name, crate, price = row.split(',')
+            cost += float(crate) * float(price)
+    return cost
+
+cost = costo_camion(FILENAME)
+
+print("Costo total {}".format(cost))
