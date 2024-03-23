6e0714eb1d80f7c36845236b28e9c986e009e7c0
Johan Revilla <johan_revilla_92@hotmail.com>
2024-03-23 00:48:28 -0300

    ejercicio 4.15 y 4.16 arboles.py terminado

--- 04_Datos/arboles.py
+++ 04_Datos/arboles.py
@@ -1,5 +1,7 @@
 import csv
 from pprint import pprint
+from collections import Counter
+import statistics
 def leer_parque(filename, parkname):
 
     with open(filename, 'rt', encoding="utf-8") as file:
@@ -15,7 +17,19 @@ def leer_parque(filename, parkname):
 def especies(trees):
     return set([tree['nombre_com'] for tree in trees])
 
+def contar_ejemplares(trees):
+    treeCount = Counter()
+
+    for tree in trees:
+        treeCount[tree['nombre_com']] += 1
+    return treeCount
+
+def obtener_alturas(trees, specieName):
+    return [ float(tree['altura_tot']) for tree in trees if tree['nombre_com'] == specieName ]
 
 trees = leer_parque('../data/arbolado-en-espacios-verdes.csv', 'GENERAL PAZ')
 #pprint(trees)
-pprint(especies(trees))
+#pprint(especies(trees))
+#pprint(contar_ejemplares(trees).most_common(5))
+pprint(max(obtener_alturas(trees, 'Jacarandá')))
+pprint(statistics.mean(obtener_alturas(trees, 'Jacarandá')))
