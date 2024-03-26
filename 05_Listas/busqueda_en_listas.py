cc1c284f3bd8b6f7baea3ea9bbed44f275ddcdba
Johan Revilla <johan_revilla_92@hotmail.com>
2024-03-26 15:59:36 -0300

    ejercicio 5.4 y 5.5 busqueda_en_listas.py terminado

--- 05_Listas/busqueda_en_listas.py
+++ 05_Listas/busqueda_en_listas.py
@@ -1,4 +1,5 @@
 
+import numpy
 def buscar_u_elemento(list, data):
     try:
         list.reverse()
@@ -6,8 +7,33 @@ def buscar_u_elemento(list, data):
     except ValueError:
         return -1
 
+def maximo(lista):
+
+    maxValue = -numpy.Infinity
+
+    for value in lista:
+        if value > maxValue:
+            maxValue = value
+
+    return maxValue
+
+def invertir_lista(list):
+    inverted_list = []
+    for data in list:
+        inverted_list.insert(0, data)
+    return inverted_list
+
 
 print(buscar_u_elemento([1,2,3,2,3,4],1))
 print(buscar_u_elemento([1,2,3,2,3,4],2))
 print(buscar_u_elemento([1,2,3,2,3,4],3))
 print(buscar_u_elemento([1,2,3,2,3,4],5))
+
+print(maximo([1,2,7,2,3,4]))
+print(maximo([1,2,3,4]))
+print(maximo([-5,4]))
+print(maximo([-5,-4]))
+print(maximo([-25,-45, -15]))
+
+print(invertir_lista([1, 2, 3, 4, 5]))
+print(invertir_lista(['Bogotá', 'Rosario', 'Santiago', 'San Fernando', 'San Miguel']))
