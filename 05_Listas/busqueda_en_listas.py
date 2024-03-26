fe3707cd9332b77a8e6d02c9103e0ad1bfbecf77
Johan Revilla <johan_revilla_92@hotmail.com>
2024-03-26 17:40:17 -0300

    ejercicio 5.6 busqueda_en_listas.py terminado

--- 05_Listas/busqueda_en_listas.py
+++ 05_Listas/busqueda_en_listas.py
@@ -23,17 +23,37 @@ def invertir_lista(list):
         inverted_list.insert(0, data)
     return inverted_list
 
+def propagate_to_right(list):
+    for index in range(len(list) - 1):
+        if list[index] == 1 and list[index + 1] == 0:
+            list[index + 1] = 1
 
-print(buscar_u_elemento([1,2,3,2,3,4],1))
-print(buscar_u_elemento([1,2,3,2,3,4],2))
-print(buscar_u_elemento([1,2,3,2,3,4],3))
-print(buscar_u_elemento([1,2,3,2,3,4],5))
+def propagate_to_left(list):
+    for index in range(len(list) -1, 0, -1):
+        if list[index] == 1 and list[index - 1] == 0:
+            list[index - 1] = 1
 
-print(maximo([1,2,7,2,3,4]))
-print(maximo([1,2,3,4]))
-print(maximo([-5,4]))
-print(maximo([-5,-4]))
-print(maximo([-25,-45, -15]))
+def propagar(list):
+    assert list != None
+    assert len(list) != 0
+    propagate_to_right(list)
+    propagate_to_left(list)
+    return list
 
-print(invertir_lista([1, 2, 3, 4, 5]))
-print(invertir_lista(['Bogotá', 'Rosario', 'Santiago', 'San Fernando', 'San Miguel']))
+
+# print(buscar_u_elemento([1,2,3,2,3,4],1))
+# print(buscar_u_elemento([1,2,3,2,3,4],2))
+# print(buscar_u_elemento([1,2,3,2,3,4],3))
+# print(buscar_u_elemento([1,2,3,2,3,4],5))
+#
+# print(maximo([1,2,7,2,3,4]))
+# print(maximo([1,2,3,4]))
+# print(maximo([-5,4]))
+# print(maximo([-5,-4]))
+# print(maximo([-25,-45, -15]))
+#
+# print(invertir_lista([1, 2, 3, 4, 5]))
+# print(invertir_lista(['Bogotá', 'Rosario', 'Santiago', 'San Fernando', 'San Miguel']))
+
+# print(propagar([ 0, 0, 0,-1, 1, 0, 0, 0,-1, 0, 1, 0, 0]))
+# print(propagar([ 0, 0, 0, 1, 0, 0]))
