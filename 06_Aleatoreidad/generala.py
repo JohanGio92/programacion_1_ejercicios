ce260de76b41bdb7032e6a2e25f2c8d4f4937401
Johan Revilla <johan_revilla_92@hotmail.com>
2024-03-27 10:09:38 -0300

    ejercicio 6.2 generala.py terminada

--- 06_Aleatoreidad/generala.py
+++ 06_Aleatoreidad/generala.py
@@ -1,15 +1,35 @@
 import random
 import numpy
 
+CANTIDAD_DE_DADOS = 5
+INTENTOS = 3
+NUMEROS_DE_CARAS = 6
 def tirar():
-    return [ random.randint(1,6) for i in range(5) ]
+    return [random.randint(1,NUMEROS_DE_CARAS) for i in range(CANTIDAD_DE_DADOS)]
 
 def es_generala(dados):
     return len(set(dados)) == 1
 
-N = 100000
+def prob_generala(REPETICIONES):
+    generalas = []
 
-G = sum([es_generala(tirar()) for i in range(N)])
-prob = G/N
-print(f'Tiré {N} veces, de las cuales {G} saqué generala servida.')
-print(f'Podemos estimar la probabilidad de sacar generala servida mediante {prob:.6f}.')
+    for i in range(REPETICIONES):
+        ok = False
+        for i in range(INTENTOS):
+            ok = es_generala(tirar())
+            if ok: break
+        generalas.append(ok)
+
+    cantidad_de_servidas = sum(generalas)
+    return cantidad_de_servidas / REPETICIONES
+
+
+
+REPETICIONES = 100000
+
+cantidad_de_servidas = sum([es_generala(tirar()) for i in range(REPETICIONES)])
+probabilidad = cantidad_de_servidas/REPETICIONES
+print(f'Tiré {REPETICIONES} veces, de las cuales {cantidad_de_servidas} saqué generala servida.')
+print(f'Podemos estimar la probabilidad de sacar generala servida mediante {probabilidad:.6f}.')
+
+print(f'El probabilidad es: {prob_generala(REPETICIONES)}')
