f1d51e21cbccea53df976693cd1d49305132f996
Johan Revilla <johan_revilla_92@hotmail.com>
2024-03-26 22:56:49 -0300

    ejercicio 5.17 arboles.py terminado

--- 05_Listas/arboles.py
+++ 05_Listas/arboles.py
@@ -9,9 +9,13 @@ def leer_parque(filename):
 
     return parks
 
-def alturas_del_arboles(trees):
-    return [float(tree['altura_tot']) for tree in trees]
+def alturas_del_arboles(trees, speciesName):
+    return [float(tree['altura_tot']) for tree in trees if speciesName == tree['nombre_com']]
+
+def alturas_por_diametro_de_los_arboles(trees, speciesName):
+    return [ tuple( [float(tree['altura_tot']), float(tree['diametro']) ]) for tree in trees if speciesName == tree['nombre_com']]
 
 trees = leer_parque('../data/arbolado-en-espacios-verdes.csv')
 # pprint(trees)
-pprint(alturas_del_arboles(trees))
+#pprint(alturas_del_arboles(trees, speciesName='Jacarandá'))
+pprint(alturas_por_diametro_de_los_arboles(trees, speciesName='Jacarandá'))
