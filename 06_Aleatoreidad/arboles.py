aae03878e55306a128f30dedb8ff7cc425041a33
Johan Revilla <johan_revilla_92@hotmail.com>
2024-03-27 12:20:08 -0300

    ejercicio 6.11 arboleada.py terminada

--- 06_Aleatoreidad/arboles.py
+++ 06_Aleatoreidad/arboles.py
@@ -2,6 +2,7 @@ import csv
 import numpy
 from pprint import pprint
 from matplotlib import pyplot as plot
+import pandas
 
 
 def leer_parque(filename):
@@ -22,11 +23,19 @@ def alturas_por_diametro_de_los_arboles(trees, specieName):
 def medidas_de_especies(trees, speciesName):
     return  { tree['nombre_com']: (float(tree['altura_tot']), float(tree['diametro'])) for tree in trees if tree['nombre_com'] in speciesName }
 
-def plotear_alturas_de_arboles(trees, speciesName):
-    alturas = numpy.array(alturas_del_arboles(trees, speciesName))
+def plotear_alturas_de_arboles(trees, specieName):
+    alturas = numpy.array(alturas_del_arboles(trees, specieName))
     plot.hist(alturas, bins= 25)
     plot.show()
 
+def plotear_alturas_por_diametro_de_los_arboles(trees, specieName):
+    alturas_por_diametros = pandas.DataFrame(alturas_por_diametro_de_los_arboles(trees, specieName), columns=['altura', 'diametro'])
+    alturas_por_diametros.plot.scatter(x='diametro', y='altura')
+    plot.xlabel("diametro (cm)")
+    plot.ylabel("alto (m)")
+    plot.title("Relación diámetro-alto para Jacarandás")
+    plot.show()
 
 trees = leer_parque('../data/arbolado-en-espacios-verdes.csv')
-plotear_alturas_de_arboles(trees, 'Jacarandá')
+# plotear_alturas_de_arboles(trees, 'Jacarandá')
+plotear_alturas_por_diametro_de_los_arboles(trees, 'Jacarandá')
